e4d62f8f5c83b80f2ec4e0095b8d22b90c0aceca
Ming Qu <ming.qu0202@gmail.com>
2018-06-27 16:50:27 +0200

    init

---
+++ exercise1/test.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+import "reflect"
+
+func main() {
+
+	var array1 [5]int
+
+	fmt.Printf("array1: %d\n\n", array1)
+
+	array2 := [5]int{12, 123, 1234, 12345, 123456}
+	array2[1] = 5000
+	fmt.Printf("array2: %d\n\n", array2[1])
+
+	var n [10]int
+	var i, j int
+
+	for i = 0; i < 10; i++ {
+		n[i] = i + 100
+	}
+
+	for j = 0; j < 10; j++ {
+		fmt.Printf("Element[%d] = %d\n", j, n[j])
+	}
+
+	var a = [5][2]int{{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}}
+	var e, f int
+
+	for e = 0; e < 5; e++ {
+		for f = 0; f < 2; f++ {
+			fmt.Printf("a[%d][%d] = %d\n", e, f, a[e][f])
+		}
+	}
+
+	fmt.Println(reflect.TypeOf(1))
+	fmt.Println(reflect.TypeOf(9.5))
+	fmt.Println(reflect.TypeOf("Just a String"))
+	fmt.Println(reflect.TypeOf(true))
+
+}
