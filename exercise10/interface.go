eaa670b8c5a5a731bba72471937349d1a491fd0f
Ming Qu <ming.qu0202@gmail.com>
2018-07-01 12:27:50 +0200

    interface

--- exercise10/interface.go
+++ exercise10/interface.go
@@ -55,16 +55,46 @@ func CsvPrinter(csv CsvParser) string {
 	return csv.Parse() + " - Date - " + csv.FormatDate()
 }
 
+type AvroParser struct {
+	Content   string
+	CreatedAt string
+}
+
+func (a AvroParser) Parse() string {
+	return fmt.Sprintf("Parsing the content in Avro: %s", a.Content)
+}
+
+func (a AvroParser) FormatDate() string {
+	return fmt.Sprintf("Formatting date for Avro: %s", a.CreatedAt)
+}
+
+type Parser interface {
+	Parse() string
+	FormatDate() string
+}
+
+func Printer(format Parser) string {
+	return format.Parse() + " - Date - " + format.FormatDate()
+}
+
 func main() {
 	xmlParser := XmlParser{Content: "<title>My Title<title>", CreatedAt: "2018-01-20"}
 	xmlPrinter := XmlPrinter(xmlParser)
 	fmt.Println(xmlPrinter)
+	fmt.Println(Printer(xmlParser))
 
 	jsonParser := JsonParser{Content: "{'title': 'My Title'}", CreatedAt: "2018-01-25"}
 	jsonPrinter := JsonPrinter(jsonParser)
 	fmt.Println(jsonPrinter)
+	fmt.Println(Printer(jsonParser))
 
 	csvParser := CsvParser{Content: "My Content; Another Content", CreatedAt: "2018-01-25"}
 	csvPrinter := CsvPrinter(csvParser)
 	fmt.Println(csvPrinter)
+	fmt.Println(Printer(csvParser))
+
+	avroParser := AvroParser{Content: "My avro content", CreatedAt: "2018-01-25"}
+	avroPrinter := Printer(avroParser)
+	fmt.Println(avroPrinter)
+
 }
